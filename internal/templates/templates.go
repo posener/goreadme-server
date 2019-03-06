304e32dfc78936cbf9c65168815aeca391421951
Eyal Posener <posener@gmail.com>
2019-03-06 20:17:12 +0200

    initial commit

---
+++ internal/templates/templates.go
@@ -0,0 +1,453 @@
+package templates
+
+import (
+	"html/template"
+	"time"
+
+	prettytime "github.com/andanhm/go-prettytime"
+	"github.com/hako/durafmt"
+)
+
+var html = template.Must(
+	template.New("html").Funcs(
+		template.FuncMap{
+			"formatDate": func(t time.Time) string {
+				return prettytime.Format(t)
+			},
+			"formatDuration": func(d time.Duration) string {
+				return durafmt.ParseShort(d).String()
+			},
+			"sha": func(sha string) string {
+				if len(sha) < 8 {
+					return sha
+				}
+				return sha[:8]
+			},
+			"color": func(status string) string {
+				switch status {
+				case "Failed":
+					return "danger"
+				case "Success":
+					return "success"
+				default:
+					return "warning"
+				}
+			},
+		}).Parse(`
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Goreadme</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+
+{{template "body" .}}
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  {{if .Error}}
+  <script>$('.alert').alert()</script>
+  {{end}}
+</body>
+</html>
+`))
+
+var base = template.Must(html.Parse(`
+{{define "body"}}
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+	<a class="navbar-brand abs" href="/">
+		<img src="https://raw.githubusercontent.com/posener/goreadme/master/icon.png" width="30" height="30" alt="">
+		Goreadme
+	</a>
+	{{ if .User }}
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="navbar-collapse collapse" id="collapsingNavbar">
+			<ul class="navbar-nav">
+				<li class="nav-item {{if .Projects}}active{{end}}">
+					<a class="nav-link" href="/projects">
+						<i class="fa fa-book" aria-hidden="true"></i>
+						Projects
+					</a>
+				</li>
+				<li class="nav-item {{if .Jobs}}active{{end}}">
+					<a class="nav-link" href="/jobs">
+						<i class="fa fa-history" aria-hidden="true"></i>
+						History
+					</a>
+				</li>
+				<li class="nav-item {{if .Repos}}active{{end}}">
+					<a class="nav-link" href="/add">
+						<i class="fa fa-play-circle" aria-hidden="true"></i>
+						Integrations
+					</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="https://github.com/settings/installations/{{.InstallID}}">
+						<i class="fa fa-wrench" aria-hidden="true"></i>
+						Manage Integration
+					</a>
+				</li>
+			</ul>
+			{{if .User}}
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<img src="{{.User.GetAvatarURL}}" width="30" height="30" class="d-inline-block align-top" alt="">
+						{{.User.GetLogin}}
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="{{.User.GetHTMLURL}}">
+							<i class="fa fa-github" aria-hidden="true"></i>
+							Github page
+						</a>
+						<a class="dropdown-item" href="/auth/logout">
+							<i class="fa fa-sign-out" aria-hidden="true"></i>
+							Logout
+						</a>
+					</div>
+				</li>
+			</ul>
+			{{end}}
+		</div>
+	{{ end }}
+</nav>
+
+	<div class="container">
+	<!-- <h3>{{block "title" .}}{{end}}</h3> -->
+
+	{{ if .Error }}
+		<div class="alert alert-danger alert-dismissible fade show" role="alert">
+			{{.Error}}
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+	{{ end }}
+
+	{{template "content" .}}
+	
+	</div>
+
+	<div class="container-fluid p-3 p-md-5 bg-light">
+		<ul class="list-inline">
+			<li class="list-inline-item">
+				Runs free on <a href="">Heroku</a>
+			</li>
+			<li class="list-inline-item">
+				Stored free on <a href="https://github.com">Github</a>
+			</li>
+			<li class="list-inline-item">
+				Written in free <a href="https://golang.org">Go</a>
+			</li>
+			<li class="list-inline-item">
+				Using free <a href="https://getbootstrap.com">Bootstrap</a>
+			</li>
+			<li class="list-inline-item">
+				Using free <a href="https://fontawesome.com">Font Awesome</a>
+			</li>
+		</ul>
+		<p>Designed anb built by Eyal Posener / 2019</p>
+		<ul class="list-inline">
+			<li class="list-inline-item"><a href="https://github.com/posener/goreadme">
+				<i class="fa fa-github" aria-hidden="true"></i>
+				Goreadme
+			</a></li>
+			<li class="list-inline-item"><a href="https://github.com/posener/goreadme-server">
+				<i class="fa fa-github" aria-hidden="true"></i>
+				Goreadme-server
+			</a></li>
+			<li class="list-inline-item"><a href="https://github.com/posener/goreadme-server/blob/master/LICENSE.txt">
+				<i class="fa fa-id-card-o" aria-hidden="true"></i>
+				MIT Licensed
+			</a></li>
+		</ul>
+  	</div>
+
+{{end}}
+`))
+
+var Home = template.Must(template.Must(base.Clone()).Parse(`
+{{define "title"}}Login{{end}}
+{{define "content"}}
+<div class="container h-75">
+	<div class="row align-items-center h-75">
+
+		<div class="col-lg-4 col-12 mx-auto">
+			<h3>Welcome</h3>
+			<p>
+				Goreadme is a Github app that automatically creates readme files from the Go doc of the project.
+				Please visit our <a href="https://github.com/apps/goreadme">Github app page</a> for more info.
+			</p>
+		</div>
+
+		{{ if not .User }}
+			<div class="col-lg-4 col-12 mx-auto">
+				<div class="jumbotron">
+					<p>
+					In order to use goreadme with your repository, login is required.
+					</p>
+					<form action="/auth/login">
+					<button type="submit" class="btn btn-outline-primary">
+						<i class="fa fa-x2 fa-github" aria-hidden="true"></i>
+						Login with Github
+					</button>
+					</form>
+				</div>
+			</div>
+		{{ end }}
+
+		<div class="col-lg-4 col-12 mx-auto list-group">
+			<h5>Our Top Open Source Users</h5>
+			{{ range .OpenSourceProjects }}
+				<a class="list-group-item list-group-item-action" href="https://github.com/{{.Owner}}/{{.Repo}}">
+					{{.Owner}}/{{.Repo}}
+				</a>
+			{{ end }}
+		</div>
+
+	</div>
+</div>
+{{end}}
+`))
+
+var headline = template.Must(base.Parse(`
+{{ define "headline" }}
+<div class="row row border-top rounded-sm bg-light">
+
+<div class="col-6 p-2 pl-3">
+	<a href="/jobs?owner={{.Owner}}&repo={{.Repo}}"><i class="fa fa-filter" aria-hidden="true"></i></a>
+	<a href="https://github.com/{{.Owner}}/{{.Repo}}"><i class="fa fa-github" aria-hidden="true"></i></a>
+	{{.Owner}}/{{.Repo}}
+</div>
+
+<div class="col-4 p-2 pl-2">
+	<div class="text-{{ color .Status }}">{{.Status}}</div>
+	{{if .PR}}
+	<div>
+		<small><a href="https://github.com/{{.Owner}}/{{.Repo}}/pull/{{.PR}}">PR#{{.PR}}</a></small>
+	</div>
+	{{end}}
+</div>
+
+<div class="col-2 p-2">
+	<form action="/add" method="post" class="float-right">
+		<input type="hidden" name="repo" value="{{.Repo}}">
+		<input type="hidden" name="owner" value="{{.Owner}}">
+		<button type="submit" class="btn btn-outline-primary btn-sm">
+			<i class="fa fa-play-circle" aria-hidden="true"></i>
+		</button>
+	</form>
+</div>
+
+</div>
+{{ end }}
+`))
+
+var branch = template.Must(base.Parse(`
+{{ define "branch" }}
+<div>
+	<a href="https://github.com/{{.Owner}}/{{.Repo}}/tree/{{.DefaultBranch}}">{{.DefaultBranch}}</a>
+</div>
+<div><small>
+	<a href="https://github.com/{{.Owner}}/{{.Repo}}/commits/{{.HeadSHA}}">{{sha .HeadSHA}}</a>
+</small></div>
+
+{{ end }}
+`))
+
+var message = template.Must(base.Parse(`
+{{ define "message" }}
+
+<i class="fa fa-quote-left fa-1x fa-pull-left fa-border" aria-hidden="true"></i>
+<small>{{.Message}}</small>
+
+{{ end }}
+`))
+
+var projectRow = template.Must(base.Parse(`
+{{ define "projectRow" }}
+<div class="row">
+<div class="col-12">
+
+{{ template "headline" . }}
+
+<div class="row mt-md-2">
+	<div class="col-lg-2 col-6 p-2">
+		{{ template "branch" . }}
+	</div>
+	<div class="col-lg-2 col-6 p-2">
+		<div>
+			<i class="fa fa-calendar" aria-hidden="true"></i>
+			{{formatDate .UpdatedAt}}
+		</div>
+		<div><small>
+			<i class="fa fa-hashtag" aria-hidden="true"></i>
+			{{.LastJob}}
+		</small></div>	
+	</div>
+
+	<div class="col-lg-8 col-12 p-2 pl-3 pr-3 p-lg-2">
+		{{ template "message" . }}
+	</div>
+
+</div>
+
+</div>
+</div>
+{{ end }}
+`))
+
+var Projects = template.Must(template.Must(base.Clone()).Parse(`
+{{define "title"}}Projects{{end}}
+{{define "content"}}
+<div class="row m-md-2">
+<div class="col-12">
+{{if .Projects}}
+		{{ range .Projects }}
+
+		{{ template "projectRow" . }}
+
+		{{ end }}
+{{else}}
+	No readmes. Please <a href="/add">add a repository</a>.
+{{end}}
+</div>
+</div>
+{{end}}
+`))
+
+var AddRepo = template.Must(template.Must(base.Clone()).Parse(`
+{{define "title"}}View Installed Repositories{{end}}
+{{define "content"}}
+{{if .Repos}}
+<div class="row">
+<div class="col-lg-6">
+<table class="table">
+{{ range .Repos }}
+<tr>
+	<td>
+			{{.GetFullName}}
+	</td>
+	<td>
+		<form action="/add" method="post">
+			<input type="hidden" name="repo" value="{{.GetName}}">
+			<input type="hidden" name="owner" value="{{.GetOwner.GetLogin}}">
+			<button type="submit" class="btn btn-outline-primary btn-sm">
+				<i class="fa fa-play-circle" aria-hidden="true"></i>
+			</button>
+		</form>
+	</td>
+</tr>
+{{ end }}
+</table>
+</div>
+</div>
+{{else}}
+No installed repositories. Please <a href="/add">add a repository</a>.
+{{end}}
+{{end}}
+`))
+
+var jobRow = template.Must(base.Parse(`
+{{ define "jobRow" }}
+<div class="row">
+<div class="col-12">
+
+{{ template "headline" . }}
+
+<div class="row mt-md-2">
+
+	<div class="col-lg-3 col-6">
+		{{ template "branch" . }}
+	</div>
+
+	<div class="col-lg-3 col-6 p-2">
+		<div>
+			<i class="fa fa-hashtag" aria-hidden="true"></i>
+			{{.Num}}
+		</div>
+		<div>
+			<i class="fa fa-calendar" aria-hidden="true"></i>
+			{{formatDate .UpdatedAt}}
+		</div>
+		<div>
+			<i class="fa fa-clock-o" aria-hidden="true"></i>
+			{{formatDuration .Duration}}
+		</div>
+	</div>
+
+	<div class="col-lg-3 col-12 p-2 pl-3 pr-3 p-lg-2">
+		{{ template "message" . }}
+	</div>
+
+</div>
+
+</div>
+</div>
+{{ end }}
+`))
+
+var JobsList = template.Must(template.Must(base.Clone()).Parse(`
+{{define "title"}}Jobs List{{end}}
+{{define "content"}}
+
+<div class="row m-md-2">
+<div class="col-12">
+{{ if .Jobs }}
+		{{ range .Jobs }}
+
+		{{ template "jobRow" . }}
+
+		{{ end }}
+{{ else }}
+	No readmes. Please <a href="/add">add a repository</a>.
+{{ end }}
+</div>
+</div>
+{{ end }}
+`))
+
+var Badge = template.Must(template.New("svg").Funcs(
+	template.FuncMap{
+		"statusColor": func(s string) string {
+			switch s {
+			case "Success":
+				return "#2ecc71"
+			case "Failed":
+				return "#d35400"
+			default:
+				return "#2e4053"
+			}
+		},
+	}).Parse(`
+<svg xmlns="http://www.w3.org/2000/svg" width="115" height="20">
+	<linearGradient id="a" x2="0" y2="100%">
+		<stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
+		<stop offset="1" stop-opacity=".1"/>
+	</linearGradient>
+	<rect rx="3" width="115" height="20" fill="#555"/>
+	<rect rx="3" x="63" width="53" height="20" fill="{{statusColor .Status}}"/>
+	<rect rx="3" width="115" height="20" fill="url(#a)"/>
+	<g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
+		<text x="32" y="15" fill="#010101" fill-opacity=".3">
+			goreadme
+		</text>
+		<text x="32" y="14">
+			goreadme
+		</text>
+		<text x="87" y="15" fill="#010101" fill-opacity=".3">
+			{{.Status}}
+		</text>
+		<text x="87" y="14">
+			{{.Status}}
+		</text>
+	</g>
+</svg>
+`))
---
+++ main.go
@@ -0,0 +1,122 @@
+// Package main is an HTTP server that works with Github hooks.
+//
+// [goreadme](github.com/posener/goreadme) is a tool for creating README.md
+// files from Go doc of a given package.
+// This project is the Github app on top of this tool. It fully automates
+// the process of keeping the README.md file updated.
+//
+// ## Usage
+//
+// 1. Go to [https://github.com/apps/goreadme](https://github.com/apps/goreadme).
+// 2. Press the "Configure" button.
+// 3. choose your account, or an organization that owns the repository.
+// 4. Review the permissions and provide access to goreadme to repositories.
+// 5. Click Save.
+//
+// You should see PRs from goreadme bot in your github repos.
+//
+// For more features, or to trigger goreadme on demand to to
+// [goreadme site](https://goreadme.herokuapp.com).
+//
+// ## How does it Work?
+//
+// Once integrated with a repository, goreadme is registered on a Github hook,
+// that calls goreadme server whenever the repository default branch is
+// modified. Goreadme then computes the new README.md file and compairs it
+// to the exiting one. If a change is needed, Goreadme will create a PR with
+// the new content of the README.md file.
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	"github.com/posener/goreadme-server/internal/auth"
+	"github.com/posener/goreadme-server/internal/githubapp"
+	"github.com/sirupsen/logrus"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var (
+	domain             = os.Getenv("DOMAIN")
+	port               = os.Getenv("PORT")
+	dbURL              = os.Getenv("DATABASE_URL")
+	sessionSecret      = os.Getenv("SESSION_SECRET")
+	githubAppID        = os.Getenv("GITHUB_APP_ID")
+	githubKey          = os.Getenv("GITHUB_KEY")
+	githubClientID     = os.Getenv("GITHUB_ID")
+	githubClientSecret = os.Getenv("GITHUB_SECRET")
+	debug              = os.Getenv("DEBUG_SERVER") == "1"
+)
+
+func main() {
+	ctx := context.Background()
+	if debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	cfg := githubapp.Config{
+		AppID:      githubAppID,
+		PrivateKey: []byte(githubKey),
+		Expires:    time.Second * 60 * 10,
+	}
+
+	client := cfg.Clients(ctx)
+	db, err := gorm.Open("postgres", dbURL)
+	if err != nil {
+		logrus.Fatalf("Connect to DB on %s: %v", dbURL, err)
+	}
+	defer db.Close()
+	if debug {
+		db.LogMode(true)
+	}
+
+	if err := db.AutoMigrate(&Job{}, &Project{}).Error; err != nil {
+		logrus.Fatalf("Migrate database: %s", err)
+	}
+
+	a := &auth.Auth{
+		SessionSecret: sessionSecret,
+		GithubID:      githubClientID,
+		GithubSecret:  githubClientSecret,
+		Domain:        domain,
+		RedirectPath:  "/auth/callback",
+		LoginPath:     "/",
+		HomePath:      "/",
+	}
+
+	a.Init()
+
+	h := &handler{
+		auth:   a,
+		db:     db,
+		github: client,
+	}
+	h.debugPR()
+
+	m := mux.NewRouter()
+	m.Methods("GET").Path("/").Handler(a.MayLogin(http.HandlerFunc(h.home)))
+	m.Methods("GET").Path("/projects").Handler(a.RequireLogin(http.HandlerFunc(h.projectsList)))
+	m.Methods("GET").Path("/jobs").Handler(a.RequireLogin(http.HandlerFunc(h.jobsList)))
+	m.Methods("POST").Path("/add").Handler(a.RequireLogin(http.HandlerFunc(h.addRepoAction)))
+	m.Methods("GET").Path("/add").Handler(a.RequireLogin(http.HandlerFunc(h.addRepo)))
+	m.Methods("GET").Path("/badge/{owner}/{repo}.svg").HandlerFunc(http.HandlerFunc(h.badge))
+	m.Methods("POST").Path("/github/hook").HandlerFunc(h.hook)
+	m.Path("/auth/login").Handler(a.LoginHandler())
+	m.Path("/auth/logout").Handler(a.LogoutHandler())
+	m.Path("/auth/callback").Handler(a.CallbackHandler())
+
+	mh := handlers.RecoveryHandler(handlers.PrintRecoveryStack(true), handlers.RecoveryLogger(logrus.StandardLogger()))(m)
+	if debug {
+		mh = handlers.LoggingHandler(logrus.StandardLogger().Writer(), mh)
+	}
+
+	logrus.Infof("Starting server...")
+	http.ListenAndServe(":"+port, mh)
+}
